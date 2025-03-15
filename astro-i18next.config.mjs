6ff4697174c8e35f777d919b99628bd98c3e6a4e
Igor Alyanoy <igoralyanoy@gmail.com>
2025-03-15 23:18:29 +0200

    feat: add content, add pages, add internationalization, remove assets

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+  locales: ['en', 'fr', 'uk'],
+  defaultLocale: 'en',
+  namespaces: ["about", "common", "home", "blog"],
+  defaultNamespace: "common",
+};
