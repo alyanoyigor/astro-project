2c148cd185fff0863551a74df39aa620afac201d
Igor Alyanoy <igoralyanoy@gmail.com>
2025-03-16 00:11:29 +0200

    feat: add deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+// @ts-ignore
 import astroI18next from 'astro-i18next';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), astroI18next()],
-
   vite: {
     plugins: [tailwindcss()],
   },
+
+  site: 'https://alyanoyigor.github.io',
+  base: import.meta.env.DEV ? '/' : 'astro-project',
 });
